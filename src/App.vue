26f37d7487fd4dbb35de251edc0ffb5ab3795170
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-01-25 11:14:39 -0300

    First Commit - clean the base of vue - implement the base of nav bar

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from '@/components/NavBar.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <NavBar />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">Wishlist</RouterLink>
+  </nav>
+</template>
+
+<style lang="css" scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  padding: 2em;
+}
+</style>
